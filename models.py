9fcb35baecdf7273162444b0e7860dbe77583a56
Jean-Christophe Lavocat <jice@lavocat.name>
2016-04-24 16:27:08 +0200

    Fix naive datetime and now use timezones

--- models.py
+++ models.py
@@ -1,13 +1,13 @@
 import datetime
 from django.db import models
 from django.db.models import Q
-
+from django.utils import timezone
 
 class ScrapedObjClass(models.Model):
     class Meta:
         verbose_name = "Scraped object class"
         verbose_name_plural = "Scraped object classes"
-        
+
     name = models.CharField(max_length=200)
     scraper_scheduler_conf = models.TextField(default='\
 "MIN_TIME": 15,\n\
@@ -22,7 +22,7 @@ class ScrapedObjClass(models.Model):
 "ZERO_ACTIONS_FACTOR_CHANGE": 5,\n\
 "FACTOR_CHANGE_FACTOR": 1.3,\n')
     comments = models.TextField(blank=True)
-    
+
     def __unicode__(self):
         return self.name
 
@@ -41,7 +41,7 @@ class ScrapedObjAttr(models.Model):
     name = models.CharField(max_length=200)
     obj_class = models.ForeignKey(ScrapedObjClass)
     attr_type = models.CharField(max_length=1, choices=ATTR_TYPE_CHOICES)
-    
+
     def __unicode__(self):
         return self.name + " (" + self.obj_class.__unicode__() + ")"
 
@@ -76,23 +76,23 @@ class Scraper(models.Model):
     pagination_type = models.CharField(max_length=1, choices=PAGINATION_TYPE, default='N')
     pagination_on_start = models.BooleanField(default=False)
     pagination_append_str = models.CharField(max_length=200, blank=True, help_text="Syntax: /somepartofurl/{page}/moreurlstuff.html")
-    pagination_page_replace = models.TextField(blank=True, 
+    pagination_page_replace = models.TextField(blank=True,
         help_text="RANGE_FUNCT: uses Python range funct., syntax: [start], stop[, step], FREE_LIST: 'Replace text 1', 'Some other text 2', 'Maybe a number 3', ...")
     checker_type = models.CharField(max_length=1, choices=CHECKER_TYPE, default='N')
     checker_x_path = models.CharField(max_length=200, blank=True)
     checker_x_path_result = models.CharField(max_length=200, blank=True)
     checker_ref_url = models.URLField(max_length=500, blank=True)
     comments = models.TextField(blank=True)
-    
+
     def get_base_elems(self):
         return self.scraperelem_set.filter(scraped_obj_attr__attr_type='B')
-    
+
     def get_base_elem(self):
         return self.scraperelem_set.get(scraped_obj_attr__attr_type='B')
-    
+
     def get_detail_page_url_elems(self):
         return self.scraperelem_set.filter(scraped_obj_attr__attr_type='U')
-    
+
     def get_detail_page_url_elem(self):
         return self.scraperelem_set.get(scraped_obj_attr__attr_type='U')
 
@@ -109,41 +109,41 @@ class Scraper(models.Model):
 
     def get_standard_update_elems_from_detail_page(self):
         return self.scraperelem_set.filter(scraped_obj_attr__attr_type='T').filter(from_detail_page=True)
-    
+
     def get_image_elems(self):
         return self.scraperelem_set.filter(scraped_obj_attr__attr_type='I')
-    
+
     def get_image_elem(self):
         return self.scraperelem_set.get(scraped_obj_attr__attr_type='I')
-    
+
     def get_scrape_elems(self):
         q1 = Q(scraped_obj_attr__attr_type='S')
         q2 = Q(scraped_obj_attr__attr_type='T')
         q3 = Q(scraped_obj_attr__attr_type='U')
         q4 = Q(scraped_obj_attr__attr_type='I')
         return self.scraperelem_set.filter(q1 | q2 | q3 | q4)
-    
+
     def get_mandatory_scrape_elems(self):
         q1 = Q(scraped_obj_attr__attr_type='S')
         q2 = Q(scraped_obj_attr__attr_type='T')
         q3 = Q(scraped_obj_attr__attr_type='U')
         q4 = Q(scraped_obj_attr__attr_type='I')
         return self.scraperelem_set.filter(q1 | q2 | q3 | q4).filter(mandatory=True)
-    
+
     def get_from_detail_page_scrape_elems(self):
         q1 = Q(from_detail_page=True)
         return self.scraperelem_set.filter(q1)
-    
+
     def __unicode__(self):
         return self.name + " (" + self.scraped_obj_class.name + ")"
-    
+
     class Meta:
         ordering = ['name', 'scraped_obj_class',]
 
 
 class ScraperElem(models.Model):
     scraped_obj_attr = models.ForeignKey(ScrapedObjAttr)
-    scraper = models.ForeignKey(Scraper)   
+    scraper = models.ForeignKey(Scraper)
     x_path = models.CharField(max_length=200)
     reg_exp = models.CharField(max_length=200, blank=True)
     from_detail_page = models.BooleanField(default=False)
@@ -158,13 +158,13 @@ class SchedulerRuntime(models.Model):
         ('C', 'CHECKER'),
     )
     runtime_type = models.CharField(max_length=1, choices=TYPE, default='P')
-    next_action_time = models.DateTimeField(default=datetime.datetime.now)
+    next_action_time = models.DateTimeField(default=timezone.now)
     next_action_factor = models.FloatField(blank=True, null=True)
     num_zero_actions = models.IntegerField(default=0)
-    
+
     def __unicode__(self):
         return unicode(self.id)
-    
+
     class Meta:
         ordering = ['next_action_time',]
 
@@ -176,7 +176,7 @@ class LogMarker(models.Model):
         ('IM', 'Important'),
         ('IG', 'Ignore'),
         ('MI', 'Miscellaneous'),
-        ('CU', 'Custom'),            
+        ('CU', 'Custom'),
     )
     message_contains = models.CharField(max_length=255)
     help_text = "Use the string format from the log messages"
@@ -202,15 +202,15 @@ class Log(models.Model):
     level = models.IntegerField(choices=LEVEL_CHOICES)
     spider_name = models.CharField(max_length=200)
     scraper = models.ForeignKey(Scraper, blank=True, null=True)
-    date = models.DateTimeField(default=datetime.datetime.now)
-    
+    date = models.DateTimeField(default=timezone.now)
+
     @staticmethod
     def numeric_level(level):
         numeric_level = 0
         for choice in Log.LEVEL_CHOICES:
             if choice[1] == level:
                 numeric_level = choice[0]
-        return numeric_level        
-    
+        return numeric_level
+
     class Meta:
         ordering = ['-date']
